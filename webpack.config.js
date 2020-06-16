d47294fa132d620796fe7b40ff907c6fbc2bd427
Michael Borcherds <github@borcherds.co.uk>
2020-06-16 15:35:17 +0100

    fix compile error " configuration has an unknown property 'mode'"

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@
 const path = require('path');
 
 module.exports = {
-  mode: 'production',
+//  mode: 'production',
   entry: {
     browser: './src/index-browser.js',
     server: './src/index-server.js'
